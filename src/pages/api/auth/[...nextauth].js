89035121015e494bbbd76c2e865c9f38c823a2ca
ykb <qodqlqodql1206@gmail.com>
2025-01-20 17:07:23 +0900

    login repair

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -5,7 +5,6 @@ import GithubProvider from "next-auth/providers/github";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { db } from "@/lib/firebase";
 import { getDocs, query, where, collection, addDoc } from "firebase/firestore";
-import { signOut } from "next-auth/react";
 
 export const authOptions = {
   secret: "968416519848645165",
@@ -19,7 +18,7 @@ export const authOptions = {
       clientSecret: process.env.NAVER_CLIENT_SECRET,
       authorization: {
         params: {
-          scope: "email name nickname birthday mobile",
+          scope: "email name nickname",
         },
       },
     }),
@@ -75,6 +74,10 @@ export const authOptions = {
               console.error("No response from Naver API");
               return false;
             }
+
+            // 요청 처리 시간을 늘리기 위해 약간의 지연 추가
+            await new Promise((resolve) => setTimeout(resolve, 500)); // 500ms 지연
+
             if (!naverProfile.email) {
               console.error("Email is missing in Naver profile");
               return false;

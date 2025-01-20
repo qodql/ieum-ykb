e1bf4413ffcf87fe8960acff45934a1056e40a9f
ykb <qodqlqodql1206@gmail.com>
2025-01-20 21:31:11 +0900

    env test

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: true, // 기존 설정 유지
+  env: {
+    NAVER_CLIENT_ID: process.env.NAVER_CLIENT_ID,
+    NAVER_CLIENT_SECRET: process.env.NAVER_CLIENT_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL || "http://localhost:3000", // 기본값 제공
+  },
 };
 
 export default nextConfig;

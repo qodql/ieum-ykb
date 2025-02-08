5c5b20d3d7cb2c61e542aca1906c59bf33d19a9d
ykb <qodqlqodql1206@gmail.com>
2025-02-08 21:17:22 +0900

    debug2

--- src/pages/page/member/Login.js
+++ src/pages/page/member/Login.js
@@ -110,17 +110,14 @@ const Login = () => {
           <div className={loginStyles.externalLoginBox}>
             <div
               onClick={() => signIn('google', { callbackUrl: '/' })}
-              style={{ backgroundImage: url(/icon/icon_login_google.svg) }}
               className={loginStyles.loginIcon}
               />
             <div
               onClick={() => signIn('github', { callbackUrl: '/' })}
-              style={{ backgroundImage: url(/icon/icon_login_git.svg) }}
               className={loginStyles.loginIcon}
               />
             <div
               onClick={handleNaverLogin} // 네이버 로그인 처리 함수 연결
-              style={{ backgroundImage: url(/icon/icon_login_naver.svg) }}
               className={loginStyles.loginIcon}
             />
           </div>
